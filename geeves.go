3388a3249e3fa982e5834b426fa69261e3513ceb
electrikmilk <brandonjordan124@gmail.com>
2022-06-28 01:37:38 -0400

    General Refactoring

--- geeves.go
+++ geeves.go
@@ -18,10 +18,9 @@ var path string
 
 // Init starts to listen and serve at port
 func Init(port string) {
-	port = fmt.Sprintf(":%s", port)
 	Logf(FYI, "Routes", "%v", Routes)
 	Logf(FYI, "Controllers", "%v", Controllers)
-	Logf(GOOD, "Ready", "Waiting for requests at http://localhost%s.", port)
+	Logf(GOOD, "Ready", "Waiting for requests at http://localhost:%s.", port)
 	go http.HandleFunc(Root, handler)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
@@ -39,14 +38,14 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	}
 	Params = request.URL.Query()
 	if len(Params) > 0 {
-		fmt.Printf("Params: %v\n", Params)
+		Logf(FYI, "Params", "%v", Params)
 	}
 	// If URL has static directory prefix
-	var static_dir string = fmt.Sprintf("/%s/", Dir)
-	if strings.HasPrefix(path, static_dir) {
-		var split_path []string = strings.Split(path, "/")
-		var static_file_path string = fmt.Sprintf("%s/%s", Dir, split_path[2])
-		_, err := os.OpenFile(static_file_path, os.O_RDONLY, 0775)
+	var staticDir string = fmt.Sprintf("/%s/", Dir)
+	if strings.HasPrefix(path, staticDir) {
+		var splitPath []string = strings.Split(path, "/")
+		var staticFilePath string = fmt.Sprintf("%s/%s", Dir, splitPath[2])
+		_, err := os.OpenFile(staticFilePath, os.O_RDONLY, 0775)
 		if errors.Is(err, os.ErrNotExist) {
 			Logf(WARN, "Failed", "404: %s", path)
 			http.Error(writer, "404 not found.", http.StatusNotFound)
@@ -54,21 +53,21 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 		}
 		// Make sure a route doesn't own this file
 		for _, route := range Routes {
-			if static_file_path == route.file {
-				Logf(WARN, "Denied", "%s belongs to route \"%s\", the correct link is %s", split_path[2], route.name, route.url)
+			if staticFilePath == route.file {
+				Logf(WARN, "Denied", "%s belongs to route \"%s\", the correct link is %s", splitPath[2], route.name, route.url)
 				http.Error(writer, "404 not found.", http.StatusNotFound)
 				return
 			}
 		}
 		// Make sure this file is not an HTML file
-		var ext string = filepath.Ext(static_file_path)
+		var ext string = filepath.Ext(staticFilePath)
 		if ext == ".html" {
-			Logf(WARN, "Denied", "%s is an HTML file, please create a route", split_path[2])
+			Logf(WARN, "Denied", "%s is an HTML file, please create a route", splitPath[2])
 			http.Error(writer, "404 not found.", http.StatusNotFound)
 			return
 		}
-		Logf(GOOD, "Static", "%s -> %s", static_file_path, split_path[2])
-		http.ServeFile(writer, request, static_file_path)
+		Logf(GOOD, "Static", "%s -> %s", staticFilePath, splitPath[2])
+		http.ServeFile(writer, request, staticFilePath)
 		return
 	}
 	// look for matching route
@@ -83,33 +82,19 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 		}
 	}
 	// Look for matching controller
-	for _, contrl := range Controllers {
-		if string(contrl.method) == method && contrl.url == path {
+	for _, controller := range Controllers {
+		if string(controller.method) == method && controller.url == path {
 			// Pass request to controller
-			Logf(GOOD, "Controller", "%s -> %s", contrl.url, contrl.name)
+			Logf(GOOD, "Controller", "%s -> %s", controller.url, controller.name)
 			// fmt.Printf("Path Params: %v\n", path_params)
-			contrl.callback(writer, request)
+			controller.callback(writer, request)
 			return
 		}
-		if string(contrl.method) != method && contrl.url == path {
-			Logf(WARN, "Denied", "Controller %s has method %s, not %s", contrl.url, contrl.method, method)
+		if string(controller.method) != method && controller.url == path {
+			Logf(WARN, "Denied", "Controller %s has method %s, not %s", controller.url, controller.method, method)
 		}
 	}
 	// Failsafe
 	Logf(WARN, "Failed", "404: %s", path)
 	http.Error(writer, "404 not found.", http.StatusNotFound)
-}
-
-func checkUrl(route string, url string) string {
-	if !strings.HasPrefix(url, "/") {
-		Logf(FYI, "Notice", "Route \"%s\" URL should begin with a slash, added automatically.", route, url)
-		url = "/" + url
-	}
-	if url != "/" && strings.HasSuffix(url, "/") {
-		Logf(FYI, "Notice", "Route \"%s\" URL does not need a trailing slash: %s", route, url)
-	}
-	if strings.Contains(url, ".") {
-		Logf(BAD, "Fatal", "Route \"%s\" URL contains a \".\" character: %s", route, url)
-	}
-	return url
 }
